8fe1e51ad27783cd3c8b5210c3d9d1d94f164e89
luanshaotong <luanshaotong@163.com>
2025-04-27 04:04:01 +0000

    update build flag

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -1200,7 +1200,7 @@ func (s *Server) startCA(caOpts *caOptions) {
 			s.RunCA(grpcServer, s.RA, caOpts)
 		} else if s.CA != nil {
 			log.Infof("Starting IstioD CA")
-			log.Infof("Build with cfg cache")
+			log.Infof("Build flag 250427v1")
 			s.RunCA(grpcServer, s.CA, caOpts)
 		}
 		return nil
